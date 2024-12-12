c704357f5c98ba0eadfdddd54c667a7f5ca60bb2
sanghee01 <sang6299@naver.com>
2024-12-12 21:32:52 +0900

    feat: 당첨 결과 출력

--- src/App.js
+++ src/App.js
@@ -60,20 +60,52 @@ class App {
       throw new Error('[ERROR] 로또 번호는 중복되면 안됩니다.');
     }
 
-    const BonusLottoInput = Number(await Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n'));
+    const bonusLottoInput = Number(await Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n'));
 
-    if (isNaN(BonusLottoInput)) {
+    if (isNaN(bonusLottoInput)) {
       throw new Error('[ERROR] 숫자 이외의 문자는 입력할 수 없습니다.');
     }
-    if (!Number.isInteger(BonusLottoInput)) {
+    if (!Number.isInteger(bonusLottoInput)) {
       throw new Error('[ERROR] 보너스 번호는 정수여야 합니다.');
     }
-    if (BonusLottoInput < 1 || BonusLottoInput > 45) {
+    if (bonusLottoInput < 1 || bonusLottoInput > 45) {
       throw new Error('[ERROR] 보너스 번호는 1 ~ 45 범위의 숫자만 입력할 수 있습니다');
     }
-    if (winningLotto.includes(BonusLottoInput)) {
+    if (winningLotto.includes(bonusLottoInput)) {
       throw new Error('[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다.');
     }
+
+    const winningCount = [0, 0, 0, 0, 0];
+    for (let lotto of lottoList) {
+      let count = 0;
+      lotto.forEach((number) => {
+        if (winningLotto.includes(number)) count++;
+      });
+
+      if (count === 6) winningCount[0]++;
+      if (count === 5 && lotto.includes(bonusLottoInput)) winningCount[1]++;
+      if (count === 5) winningCount[2]++;
+      if (count === 4) winningCount[3]++;
+      if (count === 3) winningCount[4]++;
+    }
+
+    Console.print(`\n당첨 통계`);
+    Console.print(`---`);
+    Console.print(`3개 일치 (5,000원) - ${winningCount[4]}개`);
+    Console.print(`4개 일치 (50,000원) - ${winningCount[3]}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${winningCount[2]}개`);
+    Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningCount[1]}개`);
+    Console.print(`6개 일치 (2,000,000,000원) - ${winningCount[0]}개`);
+
+    const getMoney =
+      winningCount[4] * 5_000 +
+      winningCount[3] * 50_000 +
+      winningCount[2] * 1_500_000 +
+      winningCount[1] * 30_000_000 +
+      winningCount[0] * 2_000_000_000;
+    const getMoneyRate = ((getMoney / purchaseInput) * 100).toFixed(1);
+
+    Console.print(`총 수익률은 ${getMoneyRate}%입니다.`);
   }
 }
 
