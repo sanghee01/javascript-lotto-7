24b05d09c47dd06d8b0d5e01cb474203347cc292
sanghee01 <sang6299@naver.com>
2024-12-12 21:04:20 +0900

    feat: 보너스 번호 입력 및 에러 처리

--- src/App.js
+++ src/App.js
@@ -59,6 +59,21 @@ class App {
     if (new Set(winningLotto).size !== 6) {
       throw new Error('[ERROR] 로또 번호는 중복되면 안됩니다.');
     }
+
+    const BonusLottoInput = Number(await Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n'));
+
+    if (isNaN(BonusLottoInput)) {
+      throw new Error('[ERROR] 숫자 이외의 문자는 입력할 수 없습니다.');
+    }
+    if (!Number.isInteger(BonusLottoInput)) {
+      throw new Error('[ERROR] 보너스 번호는 정수여야 합니다.');
+    }
+    if (BonusLottoInput < 1 || BonusLottoInput > 45) {
+      throw new Error('[ERROR] 보너스 번호는 1 ~ 45 범위의 숫자만 입력할 수 있습니다');
+    }
+    if (winningLotto.includes(BonusLottoInput)) {
+      throw new Error('[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다.');
+    }
   }
 }
 
