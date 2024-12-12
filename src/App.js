faa2318b44431012e55159737b668b0343635538
sanghee01 <sang6299@naver.com>
2024-12-12 20:59:08 +0900

    feat: 로또 번호 입력 및 에러 처리

--- src/App.js
+++ src/App.js
@@ -32,6 +32,33 @@ class App {
     for (let lotto of lottoList) {
       Console.print(lotto);
     }
+
+    const winningLottoInput = await Console.readLineAsync('\n당첨 번호를 입력해 주세요.\n');
+
+    if (winningLottoInput[0] === ',' || winningLottoInput.at(-1) === ',') {
+      throw new Error('[ERROR] 쉼표(,)가 잘못된 위치에 있습니다.');
+    }
+
+    const winningLotto = winningLottoInput.split(',').map(Number);
+
+    winningLotto.forEach((char) => {
+      if (isNaN(char)) {
+        throw new Error('[ERROR] 쉼표(,)와 숫자 이외의 문자는 입력할 수 없습니다.');
+      }
+      if (!Number.isInteger(char)) {
+        throw new Error('[ERROR] 로또 번호는 정수여야 합니다.');
+      }
+      if (char < 1 || char > 45) {
+        throw new Error('[ERROR] 로또 번호는 1 ~ 45 범위의 숫자만 입력할 수 있습니다');
+      }
+    });
+
+    if (winningLotto.length !== 6) {
+      throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
+    }
+    if (new Set(winningLotto).size !== 6) {
+      throw new Error('[ERROR] 로또 번호는 중복되면 안됩니다.');
+    }
   }
 }
 
